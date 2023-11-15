25c16e021d888896274ffcf7f97e973cb5357bfb
Raymond D. Lee <raydlee.mobile@gmail.com>
2023-11-15 09:17:25 +0700

    local updates

--- init.sh
+++ init.sh
@@ -1,12 +1,15 @@
-#!/data/data/com.termux/files/usr/bin/env bash
+#!/bin/sh
 
 clear
 
 [ -d "$HOME/motd" ] && MOTD="$HOME/motd"
 [ -d "$HOME/.motd" ] && MOTD="$HOME/.motd"
 [ -d "$PREFIX/motd" ] && MOTD="$PREFIX/motd"
+export MOTD
 
 for i in $(find $MOTD -maxdepth 1 -regex '.+/[0-9\-].+' -not -regex ".+\.disabled" | sort)
 do
     bash $i
 done
+
+#unset MOTD
